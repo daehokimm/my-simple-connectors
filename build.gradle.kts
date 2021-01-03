b8fac9f6e3308418770692746d859b86e8f9cd29
Daeho Kim <daehokimm@gmail.com>
2021-01-03 22:10:12 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.21")
+    implementation("org.apache.kafka:connect-api:2.4.1")
+}
